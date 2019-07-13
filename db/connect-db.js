ea0f54bc70900f47cfb5e86ea995357497cbd410
mark bashir <beejayphil@gmail.com>
2019-07-13 15:58:46 +0100

    successfully ccreated users in mysql

--- db/connect-db.js
+++ db/connect-db.js
@@ -1,14 +1,32 @@
 const mysql = require('mysql');
-	const db = mysql.createConnection({
-		host: '127.0.0.1',
-		user: 'guest',
-		password: 'password666',
-		database: 'contact'
-	});
-db.connect((error) => {
-	if (error) throw error;
-	console.log('connection successful')
+const db = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'guest',
+	password: 'password666',
+	database: 'contact'
 });
+db.connect(error => {
+	if (error) throw error;
+	console.log('connection successful');
+});
+ function createUser(firstName, lastName, email, phone, gender, blocked = 0) {
+	if (db) {
+		return new Promise((resolve, reject) => {
+			db.query(
+				'INSERT INTO users SET firstName=?,lastName=?,email=?,phone=?,gender=?,blocked=?',
+				[firstName, lastName, email, phone, gender, blocked],
+				(error, results) => {
+					if (error) return reject(error.message);
+					resolve({ id: results.insertId, affectedRows: results.affectedRows });
+				}
+			);
+		});
+	}
 
-module.exports = {};
+}
+function updateUser(id,...rest) { 
 
+}
+
+
+module.exports = { createUser };
