0df790bf37013644f9c04d5bef735d3430fd02f7
mark bashir <beejayphil@gmail.com>
2019-07-14 17:10:44 +0100

    added database query logic

--- db/connect-db.js
+++ db/connect-db.js
@@ -1,11 +1,157 @@
-const mongodb = require('mongodb');
+const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
+mongoose
+	.connect('mongodb://localhost:27017/contact', { useNewUrlParser: true })
+	.then(() => {
+		console.log('connected to mongo db');
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
 const userSchema = new mongoose.Schema({
-	firstName: { type: string, maxlenght: 20, required: true, trim: true },
-	lastName: { type: string, maxlenght: 20, required: true, trim: true },
-	phone: { type: string, lenght: 11, required: true, trim: true },
-	gender: { type: string, enum: ['male', 'female'], required: true, trim: true },
-	email: { type: string, required: true },
-	blocked: { type: Number, enum: [0, 1], required: true, default: 0 }
+	firstName: { type: String, maxlenght: 20, required: true, trim: true },
+	lastName: { type: String, maxlenght: 20, required: true, trim: true },
+	phone: { type: String, lenght: 11, required: true, trim: true },
+	gender: { type: String, enum: ['male', 'female'], required: true, trim: true },
+	email: { type: String, required: true },
+	blocked: { type: Boolean, enum: [true, false], required: true, default: 0 }
 });
-module.exports.user = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
+function createUser(firstName, lastName, phone, gender, email, blocked = false) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const user = new User({ firstName, lastName, phone, gender, email, blocked });
+			const result = await user.save();
+			resolve(result);
+		} catch (error) {
+			reject(error.message);
+		}
+	});
+}
+function getUsers() {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const users = await User.find().select({
+				firstName: 1,
+				lastName: 1,
+				gender: 1,
+				email: 1,
+				phone: 1,
+				blocked: 1
+			});
+			resolve(users);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+function getUsersNotBlocked() {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const users = await User.find({ blocked: false }).select({
+				firstName: 1,
+				lastName: 1,
+				gender: 1,
+				email: 1,
+				phone: 1,
+				blocked: 1
+			});
+			resolve(users);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+function getBlockedUsers() {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const users = await User.find({ blocked: true }).select({
+				firstName: 1,
+				lastName: 1,
+				gender: 1,
+				email: 1,
+				phone: 1,
+				blocked: 1
+			});
+			resolve(users);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+function getSingleUser(id) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const users = await User.find({ _id: id }).select({
+				firstName: 1,
+				lastName: 1,
+				gender: 1,
+				email: 1,
+				phone: 1,
+				blocked: 1
+			});
+			resolve(users);
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+function updateUser(id, details) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let userRetrieved = await singleUser(id);
+			let user = userRetrieved[0];
+			if (user) {
+				let {
+					firstName = user.firstName,
+					lastName = user.lastName,
+					gender = user.gender,
+					phone = user.phone,
+					email = user.email,
+					blocked = user.blocked
+				} = details;
+				const result = await User.updateOne(
+					{
+						_id: id
+					},
+					{
+						$set: {
+							firstName,
+							lastName,
+							gender,
+							phone,
+							email,
+							blocked
+						}
+					}
+				);
+				resolve(result);
+			} else {
+				throw new Error('id not found');
+			}
+		} catch (error) {
+			reject(error.message);
+		}
+	});
+}
+function blockUser(id) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const result = await User.updateOne(
+				{
+					_id: id
+				},
+				{
+					$set: {
+						blocked: true
+					}
+				}
+			);
+			resolve(result);
+		} catch (error) {
+			reject(error.message);
+		}
+	});
+}
+
+module.exports = { createUser, getUsers, getUsersNotBlocked, getBlockedUsers, updateUser, getSingleUser };
