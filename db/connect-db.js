457be0b8415f8d3bf68d06e3719b2652605ba202
mark bashir <beejayphil@gmail.com>
2019-07-14 18:58:31 +0100

    set up mongo

--- db/connect-db.js
+++ db/connect-db.js
@@ -1,32 +1,40 @@
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-mongoose
-	.connect('mongodb://localhost:27017/contact', { useNewUrlParser: true })
-	.then(() => {
-		console.log('connected to mongo db');
-	})
-	.catch(error => {
-		console.error(error.message);
-	});
-const userSchema = new mongoose.Schema({
-	firstName: { type: String, maxlenght: 20, required: true, trim: true },
-	lastName: { type: String, maxlenght: 20, required: true, trim: true },
-	phone: { type: String, lenght: 11, required: true, trim: true },
-	gender: { type: String, enum: ['male', 'female'], required: true, trim: true },
-	email: { type: String, required: true },
-	blocked: { type: Boolean, enum: [true, false], required: true, default: 0 }
-});
-const User = mongoose.model('user', userSchema);
-function createUser(firstName, lastName, phone, gender, email, blocked = false) {
+function connectToDatabase() {
 	return new Promise(async (resolve, reject) => {
 		try {
-			const user = new User({ firstName, lastName, phone, gender, email, blocked });
-			const result = await user.save();
-			resolve(result);
+			const dbConnect = await mongoose.connect('mongodb://localhost:27017/contact', { useNewUrlParser: true });
+
+			mongoose.set('useFindAndModify', false);
+			const userSchema = new mongoose.Schema({
+				firstName: { type: String, maxlenght: 20, required: true, trim: true },
+				lastName: { type: String, maxlenght: 20, required: true, trim: true },
+				phone: { type: String, lenght: 11, required: true, trim: true },
+				gender: { type: String, enum: ['male', 'female'], required: true, trim: true },
+				email: { type: String, required: true },
+				blocked: { type: Boolean, enum: [true, false], required: true, default: 0 }
+			});
+			const User =  mongoose.model('user', userSchema);
+			resolve({dbConnect, User });
 		} catch (error) {
 			reject(error.message);
 		}
 	});
+}
+let dbConnect = connectToDatabase();
+async function createUser(firstName, lastName, phone, gender, email, blocked = false) {
+	let connect = await dbConnect;
+	if (connect) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const user = new User({ firstName, lastName, phone, gender, email, blocked });
+				const result = await user.save();
+				resolve(result);
+			} catch (error) {
+				reject(error.message);
+			}
+		});
+	}
 }
 function getUsers() {
 	return new Promise(async (resolve, reject) => {
@@ -99,7 +107,7 @@ function getSingleUser(id) {
 function updateUser(id, details) {
 	return new Promise(async (resolve, reject) => {
 		try {
-			let userRetrieved = await singleUser(id);
+			let userRetrieved = await getSingleUser(id);
 			let user = userRetrieved[0];
 			if (user) {
 				let {
@@ -110,7 +118,7 @@ function updateUser(id, details) {
 					email = user.email,
 					blocked = user.blocked
 				} = details;
-				const result = await User.updateOne(
+				const result = await User.findOneAndUpdate(
 					{
 						_id: id
 					},
@@ -123,7 +131,8 @@ function updateUser(id, details) {
 							email,
 							blocked
 						}
-					}
+					},
+					{ new: true }
 				);
 				resolve(result);
 			} else {
@@ -137,7 +146,7 @@ function updateUser(id, details) {
 function blockUser(id) {
 	return new Promise(async (resolve, reject) => {
 		try {
-			const result = await User.updateOne(
+			const result = await User.findOneAndUpdate(
 				{
 					_id: id
 				},
@@ -145,7 +154,8 @@ function blockUser(id) {
 					$set: {
 						blocked: true
 					}
-				}
+				},
+				{ new: true }
 			);
 			resolve(result);
 		} catch (error) {
@@ -153,5 +163,27 @@ function blockUser(id) {
 		}
 	});
 }
+function deleteUser(id) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let connect = await dbConnect;
+			if (connect) {
+				let { User } = connect;
+				let result = await User.deleteOne({ _id: id });
+				resolve(result);
+			}
+		} catch (error) {
+			reject(error.message);
+		}
+	});
+}
+user = deleteUser('5d2b15aea47488275253b117');
+user
+	.then(data => {
+		console.log(data);
+	})
+	.catch(data => {
+		console.log(data);
+	});
 
-module.exports = { createUser, getUsers, getUsersNotBlocked, getBlockedUsers, updateUser, getSingleUser };
+module.exports = { createUser, getUsers, getUsersNotBlocked, getBlockedUsers, updateUser, getSingleUser, blockUser };
