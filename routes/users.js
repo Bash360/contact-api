018ae30e5382bd245b97a380855344d68af3850d
mark bashir <beejayphil@gmail.com>
2019-07-15 15:05:54 +0100

    added supertest

--- routes/users.js
+++ routes/users.js
@@ -8,29 +8,39 @@ const {
 	getBlockedUsers,
 	updateUser,
 	blockUser,
-	deleteUser
+	deleteUser,
+	searchContact
 } = require('../db/connect-db.js');
 const userRouter = express.Router();
 
 const postSchema = {
 	firstName: joi
-		.string().trim()
+		.string()
+		.trim()
 		.min(3)
 		.required(),
 	lastName: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.min(3)
 		.required(),
 	email: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.email()
 		.required(),
 	phone: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.max(11)
 		.required(),
 	gender: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.max(6)
 		.min(4)
 		.required(),
@@ -41,27 +51,44 @@ const postSchema = {
 };
 const updateSchema = {
 	firstName: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.min(3)
 		.optional(),
 	lastName: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.min(3)
 		.optional(),
 	email: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.email()
 		.optional(),
 	phone: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.max(11)
 		.optional(),
 	gender: joi
-		.string().trim().lowercase()
+		.string()
+		.trim()
+		.lowercase()
 		.max(6)
 		.min(4)
 		.optional()
 };
+const searcSchema = {
+	q: joi
+		.string()
+		.required()
+		.lowercase()
+};
+
 userRouter.get('/', (req, res) => {
 	getAllUsers()
 		.then(data => {
@@ -72,12 +99,37 @@ userRouter.get('/', (req, res) => {
 		});
 });
 userRouter.get('/search?', (req, res) => {
-
-	 getSingleUser(req.query.id).then((data) => { res.status(200).json(data); }).catch((error) => { res.status(400).json(error) });
+	let { error } = joi.validate(req.query, searcSchema);
+	if (!error) {
+			searchContact(req.query.q)
+			.then(data => {
+				res.status(200).json(data);
+			})
+			.catch(error => {
+				res.status(404).json(error);
+			});
+	} else {
+		res.status(400).json(error[0]);
+	}
 });
+userRouter.get('/:id', (req, res) => {
+	getSingleUser(req.params.id)
+		.then(data => {
+			res.status(200).json(data);
+		})
+		.catch(error => {
+			res.status(400).json(error);
+		});
+});
+
 userRouter.get('/blocked', (req, res) => {
-	getBlockedUsers().then((data) => { data.length === 0 ? res.status(200).json('no blocked user') : res.status(200).json(data); }).
-		catch((error) => {console.error(error) });
+	getBlockedUsers()
+		.then(data => {
+			data.length === 0 ? res.status(200).json('no blocked user') : res.status(200).json(data);
+		})
+		.catch(error => {
+			console.error(error);
+		});
 });
 userRouter.post('/', (req, res) => {
 	const validated = joi.validate(req.body, postSchema, { abortEarly: false });
@@ -99,27 +151,49 @@ userRouter.post('/', (req, res) => {
 	}
 });
 userRouter.put('/update/:id', (req, res) => {
-const validated = joi.validate(req.body, updateSchema, { abortEarly: false });
+	const validated = joi.validate(req.body, updateSchema, { abortEarly: false });
 	const { error } = validated;
 	if (error) {
 		let errors = error.details.reduce((errors, error) => {
 			return errors + ' \n' + error.message;
 		}, ' ');
 		return res.status(400).send(errors);
-	} else { 
-		updateUser(req.params.id, req.body).then((data) => { res.status(200).json(data)}).catch((error)=>{res.json(error)});
-	} 
+	} else {
+		updateUser(req.params.id, req.body)
+			.then(data => {
+				res.status(200).json(data);
+			})
+			.catch(error => {
+				res.json(error);
+			});
+	}
 });
 userRouter.put('/block/:id', (req, res) => {
-	blockUser(req.params.id).then((data) => { res.status(200).json(data) }).catch((error) => { res.json(error) });
+	blockUser(req.params.id)
+		.then(data => {
+			res.status(200).json(data);
+		})
+		.catch(error => {
+			res.json(error);
+		});
 });
-userRouter.get('/nonblockedusers', (req,res) => { 
-	getNonBlockedUsers().then((data) => { 
-		 data.length === 0 ? res.status(200).json('no nonblocked user') : res.status(200).json(data);
-	}).catch((error) => { res.json(error) });
+userRouter.get('/nonblockedusers', (req, res) => {
+	getNonBlockedUsers()
+		.then(data => {
+			data.length === 0 ? res.status(200).json('no nonblocked user') : res.status(200).json(data);
+		})
+		.catch(error => {
+			res.json(error);
+		});
 });
 userRouter.delete('/delete/:id', (req, res) => {
-	deleteUser(req.params.id).then((data) => { res.status(200).json({deletedUser: data, id:req.params.id}) }).catch((error) => { res.json(error) });
+	deleteUser(req.params.id)
+		.then(data => {
+			res.status(200).json({ deletedUser: data, id: req.params.id });
+		})
+		.catch(error => {
+			res.json(error);
+		});
 });
 
 module.exports = userRouter;
