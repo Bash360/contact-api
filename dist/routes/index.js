e2541e6603488634f86c8409b27a57c5129831b3
mark bashir <beejayphil@gmail.com>
2019-07-17 10:04:22 +0100

    removed files wanted to be ignored

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Contact Page' });
-});
-
-module.exports=router;
